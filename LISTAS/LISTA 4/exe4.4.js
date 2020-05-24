9e23e7559184bca088f1d24d9c523ef2e998b7e7
Hugo Felipe Amanajás Gama <felipe-amanajas@msn.com>
2020-05-24 10:18:59 -0300

    Exercício 4.4 corrigido

--- LISTAS/LISTA 4/exe4.4.js
+++ LISTAS/LISTA 4/exe4.4.js
@@ -3,19 +3,15 @@ e de 5 entre 1 e 1000. */
 
 var n1 = 1
 var n2 = 1000
-
-var listaDosMultiplos = 0
+var soma = 0
+var listaDosMultiplos = []
 
 while (n1 <= n2) {
-    if (n1 % 3 == 0) {
-        listaDosMultiplos = listaDosMultiplos + n1;
-
-    } else if (n1 % 5 == 0) {
-        listaDosMultiplos = listaDosMultiplos + n1;
+    if (n1 % 3 == 0 && n1 % 5 == 0) {
+        listaDosMultiplos.push(n1)
+        soma = soma + n1;
     }
     n1++
 }
 
-console.log('Total:', listaDosMultiplos)
-
-//como exibir os números que estão sendo somados?
+console.log(`A soma de ${listaDosMultiplos}, resulta em ${soma}`)

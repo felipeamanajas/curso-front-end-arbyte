0afb86acf1e2c654117b377e6da5a85ea93f1dde
Hugo Felipe Amanajás Gama <felipe-amanajas@msn.com>
2020-05-25 12:33:28 -0300

    Exercício 5.5 resolvido

--- LISTAS/LISTA 5/exe5.5.js
+++ LISTAS/LISTA 5/exe5.5.js
@@ -3,3 +3,44 @@ usando o comando For.. Perguntar ao usuário se ele deseja calcular o fatorial d
 outro número e repetir a execução do programa tantas vezes quantas o usuário indicar.
 Exemplo de tela de saída: */
 
+console.log('\n Este algoritmo resolve o fatorial n! de um numero!')
+var rs = require('readline-sync')
+
+for (; true;) {
+    var entrada = rs.questionInt(" \n Selecione uma opcao: \n Insira: 0 - SAIR \n Insira: 1 - RESOLVER \n ")
+
+    if (entrada == 0) {
+       break
+
+    } else if (entrada == 1) {
+        var fatorial = 1;
+        var n = rs.questionInt("Insira o n!:")
+        i = 1
+
+        for (i = 1; i <= n; i++) {
+            fatorial = fatorial * i;
+        }
+        console.log(fatorial);
+        continue
+
+    } else {
+        console.log('Escolha uma das opcoes abaixo:')
+    }
+}
+
+
+
+
+
+
+
+
+
+// var fatorial =1;
+// var n = 5
+// i = 1 
+
+// for(i=1;i<=n;i++){
+//   fatorial=fatorial*i;
+// }
+// console.log(fatorial);
